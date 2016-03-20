6b1ed7b0485f07101fb7dd59af1e0b92e7dd60a5
xiangxik <slimstream@live.cn>
2016-03-20 11:18:25 +0800

    searchPanel

--- application-centralize/src/main/resources/META-INF/app/ux/grid/SearchPanel.js
+++ application-centralize/src/main/resources/META-INF/app/ux/grid/SearchPanel.js
@@ -1,53 +1,21 @@
 Ext.define('app.ux.grid.SearchPanel', {
 	extend : "Ext.form.Panel",
 	alias : "widget.searchpanel",
-	title : "搜索面板",
-	collapsible : true,
 	border : false,
 	frame : false,
-	layout : "column",
-	bodyPadding : 5,
+	layout : "fit",
+	bodyPadding : "5 5 0 5",
 	fieldDefaults : {
 		labelAlign : "right",
 		labelWidth : 80
 	},
-	buttonAlign : "center",
-	buttons : [ {
-		text : "搜索",
-		formBind : true,
-		handler : function() {
-			var formPanel = this.up("form");
-			var form = formPanel.getForm();
-			var store = formPanel.getStore();
-
-			if (form.isValid()) {
-
-				form.getFields().each(function(item, index, len) {
-					if (item.getValue() && item.getValue() != "" && (Ext.getClassName(item) != "Ext.ux.TreePicker" || item.getValue() != "root")) {
-						this.addFilter(new Ext.util.Filter({
-							id : item.getName(),
-							property : item.getName(),
-							value : item.getValue()
-						}), true);
-					} else {
-						this.removeFilter(item.getName(), true);
-					}
-				}, store);
-
-				store.load();
-			}
-		}
-	}, {
-		text : "重置",
-		handler : function() {
-			var formPanel = this.up("form");
-			var form = formPanel.getForm();
-			var store = formPanel.getStore();
-
-			form.reset();
-			form.getFields().each(function(item, index, len) {
-				this.removeFilter(item.getName(), true);
-			}, store);
+	items : [ {
+		xtype : "fieldset",
+		title : "查询面板",
+		layout : "column",
+		collapsible : true,
+		defaults : {
+			margin : "0 0 5 0"
 		}
 	} ],
 	getStore : function() {
@@ -55,5 +23,60 @@ Ext.define('app.ux.grid.SearchPanel', {
 	},
 	setStore : function(store) {
 		this.store = store;
+	},
+	listeners : {
+		beforerender : function(form, opts) {
+			var fieldset = form.child("fieldset");
+			fieldset.add(this.queryItems);
+			fieldset.add({
+				xtype : "fieldcontainer",
+				layout : "hbox",
+				defaults:{
+					margin: "0 0 0 8"
+				},
+				items : [
+						{
+							text : "搜索",
+							xtype : "button",
+							handler : function() {
+								var formPanel = this.up("form");
+								var form = formPanel.getForm();
+								var store = formPanel.getStore();
+
+								if (form.isValid()) {
+
+									form.getFields().each(
+											function(item, index, len) {
+												if (item.getValue() && item.getValue() != ""
+														&& (Ext.getClassName(item) != "Ext.ux.TreePicker" || item.getValue() != "root")) {
+													this.addFilter(new Ext.util.Filter({
+														id : item.getName(),
+														property : item.getName(),
+														value : item.getValue()
+													}), true);
+												} else {
+													this.removeFilter(item.getName(), true);
+												}
+											}, store);
+
+									store.load();
+								}
+							}
+						}, {
+							text : "重置",
+							xtype : "button",
+							handler : function() {
+								var formPanel = this.up("form");
+								var form = formPanel.getForm();
+								var store = formPanel.getStore();
+
+								form.reset();
+								form.getFields().each(function(item, index, len) {
+									this.removeFilter(item.getName(), true);
+								}, store);
+							}
+						} ]
+			});
+		}
 	}
 });
