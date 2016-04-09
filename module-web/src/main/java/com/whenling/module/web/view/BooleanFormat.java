da1f73ffad3cb3f2c240dab0596a89c101dc60da
xiangxik <slimstream@live.cn>
2016-04-09 09:46:00 +0800

    beetl

---
+++ module-web/src/main/java/com/whenling/module/web/view/BooleanFormat.java
@@ -0,0 +1,26 @@
+package com.whenling.module.web.view;
+
+import org.beetl.core.Format;
+
+import com.google.common.base.Strings;
+
+public class BooleanFormat implements Format {
+
+	public static final String DEFAULT_PATTERN = "是,否";
+
+	@Override
+	public Object format(Object data, String pattern) {
+		if (data == null) {
+			return null;
+		}
+		if (data instanceof Boolean) {
+			if (Strings.isNullOrEmpty(pattern)) {
+				pattern = DEFAULT_PATTERN;
+			}
+			String[] trueOrFalse = pattern.split(",");
+			return ((Boolean) data) ? trueOrFalse[0] : trueOrFalse[1];
+		}
+		return null;
+	}
+
+}
