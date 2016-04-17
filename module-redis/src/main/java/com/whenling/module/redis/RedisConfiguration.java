f8f1e4c0188ae68a9a61710cbab4f77510f892e0
ken <ken@whenling.com>
2016-04-17 11:33:04 +0800

    redis

--- module-redis/src/main/java/com/whenling/module/redis/RedisConfiguration.java
+++ module-redis/src/main/java/com/whenling/module/redis/RedisConfiguration.java
@@ -1,5 +1,6 @@
 package com.whenling.module.redis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
@@ -10,21 +11,30 @@ import redis.clients.jedis.JedisPoolConfig;
 @Configuration
 public class RedisConfiguration {
 
+	@Value("${redis.host?:120.25.241.144}")
+	private String hostName;
+
+	@Value("${redis.port?:6379}")
+	private String port;
+
+	@Value("${redis.password?:b840fc02d52404542994}")
+	private String password;
+
 	@Bean
 	public JedisPoolConfig jedisPoolConfig() {
 		JedisPoolConfig config = new JedisPoolConfig();
 		return config;
 	}
-	
+
 	@Bean
 	public JedisConnectionFactory jedisConnectionFactory() {
 		JedisConnectionFactory connectionFactory = new JedisConnectionFactory(jedisPoolConfig());
-		connectionFactory.setHostName("120.25.241.144");
-		connectionFactory.setPort(6379);
-		connectionFactory.setPassword("b840fc02d52404542994");
+		connectionFactory.setHostName(hostName);
+		connectionFactory.setPort(Integer.parseInt(port));
+		connectionFactory.setPassword(password);
 		return connectionFactory;
 	}
-	
+
 	@Bean
 	public StringRedisTemplate stringRedisTemplate() {
 		StringRedisTemplate stringRedisTemplate = new StringRedisTemplate(jedisConnectionFactory());
